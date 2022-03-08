bf9ac10d859de4c4e4da4c00f5f13c28f511dd43
Riwan <coefficriwan@gmail.com>
2022-03-09 00:28:30 +0100

    mobile layout

--- app/src/routes/experiments/index.svelte
+++ app/src/routes/experiments/index.svelte
@@ -35,6 +35,6 @@
 	#wrapper {
 		@apply mx-auto grid gap-4 px-10;
 		@apply sm:max-w-2xl lg:max-w-screen-lg xl:max-w-screen-xl;
-		@apply grid-cols-2 lg:grid-cols-3 xl:grid-cols-4;
+		@apply grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4;
 	}
 </style>
